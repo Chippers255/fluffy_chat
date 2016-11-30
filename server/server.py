e7ced4f865fe0bc0ba8096b32be6ced963b3d457
Thomas Nelson <tn90ca@gmail.com>
2016-11-29 22:08:12 -0500

    Implemented Connected User Update

--- server/server.py
+++ server/server.py
@@ -7,34 +7,51 @@ import datetime
 import websockets
 
 CONNECTED_CLIENTS = []
+CLIENT_NAMES = {}
 
 auto_correct = {'engineer': 'car mechanic',
                 'bitcoin': 'magic bean',
                 'java': 'jibberish'}
 
 async def handle_message(encoded_message):
-    # Grab message time ** May want to move this to client side **
     message_received_time = datetime.datetime.now().strftime("%H:%M")
 
     decoded_message = ujson.loads(encoded_message)
 
     id = decoded_message['id']
     name = decoded_message['name']
-    message = decoded_message['message']
+    message = decoded_message[ 'message']
 
     msg = {'id': id,
            'name': name,
            'message': message,
            'time': message_received_time}
+
     msg = ujson.dumps(msg)
+
     for ws in CONNECTED_CLIENTS:
         await ws.send(msg)
+
     print(msg)
 # end def handle_message
 
 
+async def update_users():
+    msg = {'type': 'update_users',
+           'id': 'server',
+           'users': []}
+
+    for name in CLIENT_NAMES.values():
+        msg['users'].append(name)
+
+    msg = ujson.dumps(msg)
+
+    for ws in CONNECTED_CLIENTS:
+        await ws.send(msg)
+# end def update_users
+
 async def connection(websocket, path):
-    global CONNECTED_CLIENTS
+    global CONNECTED_CLIENTS, CLIENT_NAMES
 
     # Grab user connection time
     start_time = datetime.datetime.now().strftime("%H:%M")
@@ -42,13 +59,18 @@ async def connection(websocket, path):
     # Add newly connected client to global connection list
     CONNECTED_CLIENTS.append(websocket)
 
-    # Wait for client name to be passed through and extract username
+    # Wait for client name to be passed through and extract user connection info
     login = await websocket.recv()
-    name = ujson.loads(login)['name']
-    id = ujson.loads(login)['id']
+    con_info = ujson.loads(login)
+    type = con_info['type']
+    id = con_info['id']
+    name = con_info['name']
 
-    #for ws in CONNECTED_CLIENTS:
-    #    await ws.send(str(start_server) + '  -  ' + str(name) + " Joined the Conversation")
+    # Add to list of usernames
+    CLIENT_NAMES[id] = name
+
+    await update_users()
+
     print(start_time + '  -  ' + name + " Joined the Conversation")  # Log to server
 
     # Loop waiting on client messages until disconnection
@@ -61,8 +83,10 @@ async def connection(websocket, path):
             end_time = datetime.datetime.now().strftime("%H:%M")
 
             CONNECTED_CLIENTS.remove(websocket)
-            #for ws in CONNECTED_CLIENTS:
-            #    await ws.send(end_time + '  -  ' + name + " Left the Conversation")
+            del CLIENT_NAMES[id]
+
+            await update_users()
+
             print(end_time + '  -  ' + name + " Left the Conversation")
             break
 # end def connection
