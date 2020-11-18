c7382ba046f2a7701238a6fc373f673586c12a85
Chippers255 <tn90ca@gmail.com>
2020-11-18 00:02:17 -0500

    cleaned up repo

--- old/client/chrome_app/background.js
+++
@@ -1,9 +0,0 @@
-/**
- * Listens for the app launching then creates the window
- */
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
-    id: 'main',
-    outerBounds: { width: 300, height: 200 }
-  });
-});
